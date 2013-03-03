349532604ad2d0302e7b7501ecb2063e04faf66e
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-03-03 19:38:11 +0100

    Adding a last exchange to quit the client loop

--- src/client.cpp
+++ src/client.cpp
@@ -11,46 +11,55 @@ client_t::~client_t (void)
 
 int client_t::loop (void)
 {
-   int ret = EXIT_SUCCESS;
+    int  ret     = EXIT_SUCCESS;
+    bool running = true;
 
-   std::cout << "client_t::loop\n";
+    std::cout << "client_t::loop\n";
 
-   try
-   {
-      // socket to request files to the server
-      std::cout << "binding...\n";
-      zmq::socket_t requester(context, zmq::REQ);
-      requester.connect ("tcp://localhost:5555");
+    try
+    {
+        // socket to request files to the server
+        std::cout << "binding...\n";
+        zmq::socket_t requester(context, zmq::REQ);
+        requester.connect ("tcp://localhost:5555");
 
-      do
-      {
-         // send "get" request
-         zmq::message_t request("get", strlen("get"));
-         std::cout << "request(" << request.size () << ")=" << request.string ().c_str () << std::endl;
-         requester.send (request);
+        do
+        {
+            // send "get" request
+            zmq::message_t request("get", strlen("get"));
+            std::cout << "request(" << request.size () << ")=" << request.string ().c_str () << std::endl;
+            requester.send (request);
       
-         // wait for the filename reply message
-         std::cout << "waiting for a filename reply\n";
-         zmq::message_t reply;
-         requester.recv (reply);
-         std::cout << "reply(" << reply.size () << ")=" << reply.string ().c_str () << std::endl;
-         // then, its size,
-         zmq::message_t reply_part2;
-         requester.recv (reply_part2);
-         std::cout << "reply_part2(" << reply_part2.size () << ")=" << ntohl(*(u_long*)reply_part2.data ()) << std::endl;
-         // and finaly its content
-         zmq::message_t reply_part3;
-         requester.recv (reply_part3);
-         std::cout << "reply_part3(" << reply_part3.size () << ")=" << reply_part3.string ().c_str () << std::endl;
-         // TODO fopen and fwrite
-         // TODO quit the loop
-      } while (true);
-   }
-   catch (std::exception& e)
-   {
-      std::cout << e.what () << std::endl;
-      ret = EXIT_FAILURE;
-   }
+            // wait for the filename reply message
+            std::cout << "waiting for a filename reply\n";
+            zmq::message_t reply;
+            requester.recv (reply);
+            std::cout << "reply(" << reply.size() << ")=" << reply.string ().c_str () << std::endl;
+            if (0 < reply.size())
+            {
+                // then, its size,
+                zmq::message_t reply_part2;
+                requester.recv (reply_part2);
+                std::cout << "reply_part2(" << reply_part2.size () << ")=" << ntohl(*(u_long*)reply_part2.data ()) << std::endl;
+                // and finaly its content
+                zmq::message_t reply_part3;
+                requester.recv (reply_part3);
+                std::cout << "reply_part3(" << reply_part3.size () << ")=" << reply_part3.string ().c_str () << std::endl;
+                // TODO fopen and fwrite
+            }
+            else
+            {
+                // quit the loop
+                running = false;
+            }
+        } while (running);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what () << std::endl;
+        ret = EXIT_FAILURE;
+    }
+    std::cout << "quitting\n";
 
-   return ret;
+    return ret;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -24,79 +24,91 @@ server_t::~server_t (void)
 
 int server_t::loop (void)
 {
-   int            ret = EXIT_SUCCESS;
-   DIR*           dir;
-   struct dirent* ent;
+    int            ret = EXIT_SUCCESS;
+    DIR*           dir;
+    struct dirent* ent;
 
-   std::cout << "server_t::loop\n";
+    std::cout << "server_t::loop\n";
 
-   if ((dir = opendir (_dir_name)) != NULL)
-   {
-      // socket to reply to client requests
-      std::cout << "binding...\n";
-      zmq::socket_t receiver(context, zmq::REP);
-      receiver.bind ("tcp://*:5555");
+    if ((dir = opendir (_dir_name)) != NULL)
+    {
+        // socket to reply to client requests
+        std::cout << "binding...\n";
+        zmq::socket_t receiver(context, zmq::REP);
+        receiver.bind ("tcp://*:5555");
 
-      // print all the files and directories within directory
-      while ((ent = readdir (dir)) != NULL)
-      {
-         // filter les répertoires "." et ".."
-         if ('.' != ent->d_name[0])
-         {
-            // wait for a request message
-            std::cout << "waiting for a get message...\n";
-            zmq::message_t request;
-            receiver.recv (request);
-            std::cout << "request(" << request.size () << ")=" << request.string ().c_str () << std::endl;
-
-            std::cout << "filename=" << ent->d_name << std::endl;
-
-            // reply with the name of the file found
-            zmq::message_t reply(ent->d_name, strlen(ent->d_name));
-            std::cout << "reply(" << reply.size () << ")=" << reply.string ().c_str () << std::endl;
-            receiver.send (reply, zmq::SNDMORE);
-
-            // add the size of the file found
-            std::string filename = _dir_name;
-            filename += ent->d_name;
-            struct stat st;
-            int ok = stat(filename.c_str (), &st);
-            if (0 == ok)
+        // print all the files and directories within directory
+        while ((ent = readdir (dir)) != NULL)
+        {
+            // filter les répertoires "." et ".."
+            if ('.' != ent->d_name[0])
             {
-                u_long val = htonl(st.st_size);
-                zmq::message_t reply_part2((void*)&val, sizeof(st.st_size));
-                std::cout << "reply_part2(" << reply_part2.size () << ")=" << st.st_size << std::endl;
-                receiver.send (reply_part2, zmq::SNDMORE);
-            }
-            else
-            {
-                abort();
-            }
+                // wait for a request message
+                std::cout << "waiting for a get message...\n";
+                zmq::message_t request;
+                receiver.recv (request);
+                std::cout << "request(" << request.size () << ")=" << request.string ().c_str () << std::endl;
 
-            // add the content of the file found
-            FILE* fp = fopen(filename.c_str (), "rb");
-            if (NULL != fp)
-            {
-                zmq::message_t reply_part3(st.st_size);
-                fread(reply_part3.data (), 1, st.st_size, fp);
-                fclose (fp);
-                std::cout << "reply_part3(" << reply_part3.size () << ")" << std::endl;
-                receiver.send (reply_part3);
-            }
-            else
-            {
-                abort();
-            }
-         }
-      }
-      closedir (dir);
-   }
-   else
-   {
-      // could not open directory
-      perror ("");
-      ret = EXIT_FAILURE;
-   }
+                std::cout << "filename=" << ent->d_name << std::endl;
 
-   return ret;
+                // reply with the name of the file found
+                zmq::message_t reply(ent->d_name, strlen(ent->d_name));
+                std::cout << "reply(" << reply.size () << ")=" << reply.string ().c_str () << std::endl;
+                receiver.send (reply, zmq::SNDMORE);
+
+                // add the size of the file found
+                std::string filename = _dir_name;
+                filename += ent->d_name;
+                struct stat st;
+                int ok = stat(filename.c_str (), &st);
+                if (0 == ok)
+                {
+                    u_long val = htonl(st.st_size);
+                    zmq::message_t reply_part2((void*)&val, sizeof(st.st_size));
+                    std::cout << "reply_part2(" << reply_part2.size () << ")=" << st.st_size << std::endl;
+                    receiver.send (reply_part2, zmq::SNDMORE);
+                }
+                else
+                {
+                    abort();
+                }
+
+                // add the content of the file found
+                FILE* fp = fopen(filename.c_str (), "rb");
+                if (NULL != fp)
+                {
+                    zmq::message_t reply_part3(st.st_size);
+                    fread(reply_part3.data (), 1, st.st_size, fp);
+                    fclose (fp);
+                    std::cout << "reply_part3(" << reply_part3.size () << ")" << std::endl;
+                    receiver.send (reply_part3);
+                }
+                else
+                {
+                    abort();
+                }
+            }
+        }
+        closedir (dir);
+
+        // wait for a last request message
+        std::cout << "waiting for a last get message...\n";
+        zmq::message_t request;
+        receiver.recv (request);
+        std::cout << "request(" << request.size () << ")=" << request.string ().c_str () << std::endl;
+
+        // Then, tell the client to quit
+        zmq::message_t reply(0);
+        std::cout << "end reply(" << reply.size () << ")=" << reply.string ().c_str () << std::endl;
+        receiver.send (reply);
+    }
+    else
+    {
+        // could not open directory
+        perror ("could not open directory");
+        ret = EXIT_FAILURE;
+    }
+    std::cout << "quitting\n";
+
+    return ret;
 }
